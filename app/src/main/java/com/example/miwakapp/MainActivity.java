a41093619c2fcc9fe530b25e68d19db05cfd42ae
swapan <swapan.d83@gmail.com>
2022-12-01 17:15:55 +0600

    Starter code

---
+++ app/src/main/java/com/example/miwakapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.miwakapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+    }
+}
